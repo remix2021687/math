aad0d6c36746531c44c63068cd0c140e31ade97b
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-05-25 21:29:27 +0200

    fixbug

--- mine.py
+++ mine.py
@@ -1,28 +1,26 @@
 import random
 
-whiles = True
+Questionsmuch = int(input("Сколько примеров задать?: "))
+Questions = 0
 good = 0
 bad = 0
 
-while whiles:
+while True:
     first_num = random.randrange(1, 10)
     secend_num = random.randrange(1, 10)
-
 
     answer = int(input(f"{first_num} * {secend_num} = "))
     control = first_num * secend_num
 
     if answer == control:
         good += 1
+        Questions += 1
 
-    if answer == "Exit":
-        whiles = False
-
-
-
-print(f"Ваши правильные ответы: {good}")
-print(f"Ваши неправильные ответы: {bad}")
+    if Questions == Questionsmuch:
+        print("==================================")
+        print(f"Ваши правильные ответы: {good}")
+        print(f"Ваши неправильные ответы: {bad}")
+        print("==================================")
+        break
 
 input()
-
-print(f"{type(good)}, {good}")
